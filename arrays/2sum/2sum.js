62357f200dd8871fda10747d236fe5b913a94091
Saul Maldonado <samaldonado4@gmail.com>
2021-01-07 23:30:38 -0600

    feat: Add js solution

---
+++ arrays/2sum/2sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] !== undefined) {
+      return [map[target - nums[i]], i];
+    }
+    map[nums[i]] = i;
+  }
+  return [];
+}
