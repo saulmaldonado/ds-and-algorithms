08497811c4cbb2ebba2f9200e76ac417207c9e09
Saul Maldonado <samaldonado4@gmail.com>
2021-01-10 17:47:05 -0600

    feat: Add js solution

---
+++ math/reverse-integer/reverse-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+function reverse(x) {
+  const max = 2 ** 31 - 1;
+  const min = -(2 ** 31);
+
+  let rev = 0;
+
+  while (x !== 0) {
+    let add = x % 10;
+    x = ~~(x / 10);
+
+    if (rev > max / 10 || (rev == max / 10 && add > 7)) {
+      return 0;
+    }
+
+    if (rev < min / 10 || (rev == min / 10 && add < -8)) {
+      return 0;
+    }
+
+    rev = rev * 10 + add;
+  }
+
+  return rev;
+}
