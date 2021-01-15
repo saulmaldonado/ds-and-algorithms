31fef4c742142b7880e3927860e356c9cc2832a1
Saul Maldonado <samaldonado4@gmail.com>
2021-01-15 14:49:03 -0600

    feat: Add java solution

---
+++ dynamic-programming/minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+  public int minPathSum(int[][] grid) {
+    int h = grid.length;
+    int w = grid[0].length;
+    int[][] dp = new int[h][w];
+
+    dp[0][0] = grid[0][0];
+
+    for (int i = 1; i < w; i++) {
+      dp[0][i] = dp[0][i - 1] + grid[0][i];
+    }
+
+    for (int i = 1; i < h; i++) {
+      dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+
+    for (int i = 1; i < h; i++) {
+      for (int j = 1; j < w; j++) {
+        dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+      }
+    }
+
+    return dp[h - 1][w - 1];
+  }
+}
