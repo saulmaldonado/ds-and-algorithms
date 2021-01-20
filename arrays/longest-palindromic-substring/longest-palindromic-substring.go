e8936942e949ad2dbccb05409ad7223cce7388af
Saul Maldonado <samaldonado4@gmail.com>
2021-01-19 21:19:28 -0600

    feat: Add new go solution

--- arrays/longest-palindromic-substring/longest-palindromic-substring.go
+++ arrays/longest-palindromic-substring/longest-palindromic-substring.go
@@ -35,3 +35,77 @@ func findLongest(s string, i int, j int) int {
 
 	return j - i - 1
 }
+
+func longestPalindrome2(s string) string {
+	s = insertHashes(s)
+
+	lengths := make([]int, len(s))
+
+	center := 0
+	right := 0
+	maxIndex := 0
+
+	for i := range s {
+		mirror := 2*center - i
+
+		if i < right {
+			if lengths[mirror] < right-i {
+				lengths[i] = lengths[mirror]
+			} else {
+				lengths[i] = right - i
+			}
+		}
+
+		lengths[i] = findLongest2(s, i, lengths[i])
+
+		if i+lengths[i] > right {
+			center = i
+			right = i + lengths[i]
+		}
+
+		if lengths[i] > lengths[maxIndex] {
+			maxIndex = i
+		}
+	}
+
+	start := maxIndex - lengths[maxIndex]
+	end := maxIndex + lengths[maxIndex]
+
+	return removeHashes(s[start : end+1])
+}
+
+func insertHashes(s string) string {
+	chars := []byte{}
+
+	for i := range s {
+		chars = append(chars, '#')
+		chars = append(chars, s[i])
+	}
+
+	chars = append(chars, '#')
+
+	return string(chars)
+}
+
+func removeHashes(s string) string {
+	chars := []byte{}
+
+	for i := range s {
+		if s[i] != '#' {
+			chars = append(chars, s[i])
+		}
+	}
+	return string(chars)
+}
+
+func findLongest2(s string, i int, length int) int {
+	start := i - length - 1
+	end := i + length + 1
+
+	for start >= 0 && end < len(s) && s[start] == s[end] {
+		length++
+		start--
+		end++
+	}
+	return length
+}
