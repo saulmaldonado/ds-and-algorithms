3bfcc47c616c25adde0d74dd0199a672bb9cca38
Saul Maldonado <samaldonado4@gmail.com>
2021-01-19 21:19:08 -0600

    feat: Add new java solution

--- arrays/longest-palindromic-substring/longest-palindromic-substring.java
+++ arrays/longest-palindromic-substring/longest-palindromic-substring.java
@@ -29,3 +29,82 @@ class Solution {
     return j - i - 1;
   }
 }
+
+class Solution {
+  public String longestPalindrome(String s) {
+    s = insertHashes(s);
+
+    int[] lengths = new int[s.length()];
+
+    int center = 0;
+    int right = 0;
+    int maxIndex = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      int mirror = 2 * center - i;
+
+      if (i < right) {
+        lengths[i] = Math.min(lengths[mirror], right - i);
+      }
+
+      lengths[i] = findLongest(s, i, lengths[i]);
+
+      if (i + lengths[i] > right) {
+        center = i;
+        right = i + lengths[i];
+      }
+
+      if (lengths[i] > lengths[maxIndex]) {
+        maxIndex = i;
+      }
+
+    }
+
+    int start = maxIndex - lengths[maxIndex];
+    int end = maxIndex + lengths[maxIndex];
+
+    return removeHashes(s.substring(start, end + 1));
+  }
+
+  public String removeHashes(String s) {
+    List<Character> chars = new ArrayList<>();
+    for (int i = 1; i < s.length(); i += 2) {
+      chars.add(s.charAt(i));
+    }
+    StringBuilder builder = new StringBuilder(chars.size());
+
+    for (Character c : chars) {
+      builder.append(c);
+    }
+
+    return builder.toString();
+  }
+
+  public String insertHashes(String s) {
+    List<Character> chars = new ArrayList<>();
+
+    for (char c : s.toCharArray()) {
+      chars.add('#');
+      chars.add(c);
+    }
+    chars.add('#');
+
+    StringBuilder builder = new StringBuilder(chars.size());
+
+    for (Character c : chars) {
+      builder.append(c);
+    }
+    return builder.toString();
+  }
+
+  public int findLongest(String s, int i, int length) {
+    int start = i - length - 1;
+    int end = i + length + 1;
+    while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+      length++;
+      start--;
+      end++;
+    }
+    return length;
+  }
+}
