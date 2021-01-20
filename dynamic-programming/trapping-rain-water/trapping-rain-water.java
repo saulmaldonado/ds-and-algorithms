b9f44db329810daf2f587a7e81422a2a402c362a
Saul Maldonado <samaldonado4@gmail.com>
2021-01-19 23:57:02 -0600

    feat: Add new java solution

--- dynamic-programming/trapping-rain-water/trapping-rain-water.java
+++ dynamic-programming/trapping-rain-water/trapping-rain-water.java
@@ -21,3 +21,41 @@ class Solution {
     return ans;
   }
 }
+
+class Solution {
+  public int trap(int[] height) {
+    if (height.length < 3) {
+      return 0;
+    }
+
+    int i = 0;
+    int j = height.length - 1;
+
+    int iMax = 0;
+    int jMax = 0;
+
+    int ans = 0;
+
+    while (i < j) {
+      if (height[i] < height[j]) {
+        if (height[i] > iMax) {
+          iMax = height[i];
+        } else {
+          int diff = iMax > height[i] ? iMax - height[i] : 0;
+          ans += diff;
+        }
+        i++;
+      } else {
+        if (height[j] > jMax) {
+          jMax = height[j];
+        } else {
+          int diff = jMax > height[j] ? jMax - height[j] : 0;
+          ans += diff;
+        }
+        j--;
+      }
+    }
+
+    return ans;
+  }
+}
