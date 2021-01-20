70e2c0eea15b15f9ec42b7fbce09df14c57e1862
Saul Maldonado <samaldonado4@gmail.com>
2021-01-19 21:19:41 -0600

    feat: Add new js solution

--- arrays/longest-palindromic-substring/longest-palindromic-substring.js
+++ arrays/longest-palindromic-substring/longest-palindromic-substring.js
@@ -31,3 +31,67 @@ function findLongest(s, i, j) {
 
   return j - i - 1;
 }
+
+function longestPalindrome2(s) {
+  s = insertHashes(s);
+
+  const lengths = new Array(s.length).fill(0);
+
+  let center = 0;
+  let right = 0;
+  let maxIndex = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let mirror = 2 * center - i;
+
+    if (i < right) {
+      lengths[i] = Math.min(lengths[mirror], right - i);
+    }
+
+    lengths[i] = findLongest2(s, i, lengths[i]);
+
+    if (i + lengths[i] > right) {
+      center = i;
+      right = i + lengths[i];
+    }
+
+    if (lengths[i] > lengths[maxIndex]) {
+      maxIndex = i;
+    }
+  }
+
+  const start = maxIndex - lengths[maxIndex];
+  const end = maxIndex + lengths[maxIndex];
+
+  return removeHashes(s.substring(start, end + 1));
+}
+
+function insertHashes(s) {
+  const chars = [];
+
+  for (let i = 0; i < s.length; i++) {
+    chars.push('#');
+    chars.push(s[i]);
+  }
+  chars.push('#');
+
+  return chars.join('');
+}
+
+function removeHashes(s) {
+  return s
+    .split('')
+    .filter((c) => c !== '#')
+    .join('');
+}
+
+function findLongest2(s, i, length) {
+  let start = i - length - 1;
+  let end = i + length + 1;
+  while (start >= 0 && end < s.length && s[start] === s[end]) {
+    length++;
+    start--;
+    end++;
+  }
+  return length;
+}
