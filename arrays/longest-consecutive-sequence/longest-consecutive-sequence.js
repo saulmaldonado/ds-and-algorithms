59801aaa06666ae57a11da9db513390ffec43176
Saul Maldonado <samaldonado4@gmail.com>
2021-01-21 00:02:30 -0600

    feat: Add js solution

---
+++ arrays/longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,26 @@
+// Sorting
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestConsecutive(nums) {
+  if (!nums.length) return 0;
+
+  nums.sort((a, b) => a - b);
+
+  let max = 0;
+  let maxSeq = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] === 1) {
+      maxSeq++;
+    } else if (nums[i] === nums[i - 1]) {
+      continue;
+    } else {
+      max = Math.max(max, maxSeq);
+      maxSeq = 1;
+    }
+  }
+  max = Math.max(max, maxSeq);
+
+  return max;
+}
