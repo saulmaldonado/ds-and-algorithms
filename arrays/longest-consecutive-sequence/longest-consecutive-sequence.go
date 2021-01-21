b06c2e684edc1b4a3b16c165bba2baf8e58b6325
Saul Maldonado <samaldonado4@gmail.com>
2021-01-21 00:03:38 -0600

    feat: Add new go solution

--- arrays/longest-consecutive-sequence/longest-consecutive-sequence.go
+++ arrays/longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -32,3 +32,39 @@ func longestConsecutive(nums []int) int {
 
 	return max
 }
+
+// Set
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	set := map[int]bool{}
+
+	for _, n := range nums {
+		set[n] = true
+	}
+
+	max := 0
+	maxSeq := 1
+
+	for i := 0; i < len(nums); i++ {
+		if _, ok := set[nums[i]-1]; !ok {
+			num := nums[i]
+
+			for set[num+1] {
+				maxSeq++
+				num++
+			}
+			if maxSeq > max {
+				max = maxSeq
+			}
+			maxSeq = 1
+		}
+	}
+	if maxSeq > max {
+		max = maxSeq
+	}
+
+	return max
+}
