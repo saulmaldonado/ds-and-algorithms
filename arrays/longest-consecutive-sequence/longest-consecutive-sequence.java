8ec68aeaaddc0f80d47fa6abee7fbee87f396ba3
Saul Maldonado <samaldonado4@gmail.com>
2021-01-21 00:03:27 -0600

    feat: Add new java solution

--- arrays/longest-consecutive-sequence/longest-consecutive-sequence.java
+++ arrays/longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -26,3 +26,37 @@ class Solution {
 
   }
 }
+
+// Set
+class Solution {
+  public int longestConsecutive(int[] nums) {
+    if (nums.length == 0)
+      return 0;
+
+    Set<Integer> set = new HashSet<>();
+
+    for (int n : nums) {
+      set.add(n);
+    }
+
+    int max = 0;
+
+    int maxSeq = 1;
+    for (int n : nums) {
+      if (!set.contains(n - 1)) {
+        int num = n;
+        while (set.contains(num + 1)) {
+          maxSeq++;
+          num++;
+        }
+        max = Math.max(max, maxSeq);
+        maxSeq = 1;
+      }
+    }
+
+    max = Math.max(max, maxSeq);
+
+    return max;
+
+  }
+}
