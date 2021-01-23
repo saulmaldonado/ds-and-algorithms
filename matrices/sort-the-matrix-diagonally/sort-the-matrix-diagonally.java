5ab90590db3beb9a7b4fd8d947561c5b5462d343
Saul Maldonado <samaldonado4@gmail.com>
2021-01-23 17:01:05 -0600

    feat: Add new java solution

--- matrices/sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
+++ matrices/sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -41,3 +41,38 @@ class Solution {
     }
   }
 }
+
+// heap
+class Solution {
+  public int[][] diagonalSort(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
+
+    Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+
+        int diff = i - j;
+
+        map.putIfAbsent(diff, new PriorityQueue<Integer>());
+
+        PriorityQueue<Integer> queue = map.get(diff);
+
+        queue.add(mat[i][j]);
+      }
+    }
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        int diff = i - j;
+
+        PriorityQueue<Integer> queue = map.get(diff);
+
+        mat[i][j] = queue.poll();
+      }
+    }
+
+    return mat;
+  }
+}
