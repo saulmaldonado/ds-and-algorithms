f5107c1d65c247e817f556d9be280215c770c331
Saul Maldonado <samaldonado4@gmail.com>
2021-01-25 17:19:25 -0600

    feat: Add new java solution

--- arrays/check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ arrays/check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -14,3 +14,43 @@ class Solution {
     return true;
   }
 }
+
+class Solution {
+  public boolean kLengthApart(int[] nums, int k) {
+
+    int x = 0;
+
+    for (int n : nums) {
+      x = (x << 1); // push existing bits in x to left
+      x = x | n; // flip first bit to 1 if n is 1
+    }
+
+    // edge case for array of 0s or k = 0
+    if (x == 0 || k == 0) {
+      return true;
+    }
+
+    // remove trailing 0s
+    while ((x & 1) == 0) { // while last bit is 0
+      x = x >> 1; // shift bits to left removing the first bit
+    }
+
+    while (x != 1) { // shift bits right until there is only one left
+      x = x >> 1; // shift to right
+
+      int count = 0;
+
+      while ((x & 1) == 0) { // shift 0s between 1s counting along the way
+        x = x >> 1;
+        count++;
+      }
+
+      if (count < k) { // check is count is equal to or greater than k
+        return false;
+      }
+    }
+
+    return true;
+
+  }
+}
