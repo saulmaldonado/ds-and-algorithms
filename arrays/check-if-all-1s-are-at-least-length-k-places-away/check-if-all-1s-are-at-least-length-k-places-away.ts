3445f1e9566f85cb89b340fe96124e81515cad7d
Saul Maldonado <samaldonado4@gmail.com>
2021-01-25 17:20:13 -0600

    feat: Add new ts solution

--- arrays/check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.ts
+++ arrays/check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.ts
@@ -12,3 +12,43 @@ function kLengthApart(nums: number[], k: number): boolean {
 
   return true;
 }
+
+function kLengthApart2(nums: number[], k: number): boolean {
+  let x: number = 0;
+
+  for (let n of nums) {
+    x = x << 1; // push existing bits in x to left
+    x = x | n; // flip first bit to 1 if n is 1
+  }
+
+  // edge case for array of 0s or k = 0
+  if (x == 0 || k == 0) {
+    return true;
+  }
+
+  // remove trailing 0s
+  while ((x & 1) == 0) {
+    // while last bit is 0
+    x = x >> 1; // shift bits to left removing the first bit
+  }
+
+  while (x != 1) {
+    // shift bits right until there is only one left
+    x = x >> 1; // shift to right
+
+    let count: number = 0;
+
+    while ((x & 1) == 0) {
+      // shift 0s between 1s counting along the way
+      x = x >> 1;
+      count++;
+    }
+
+    if (count < k) {
+      // check is count is equal to or greater than k
+      return false;
+    }
+  }
+
+  return true;
+}
