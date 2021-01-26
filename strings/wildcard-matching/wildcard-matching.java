dbb882668948b9eae93086f80494cffeca4e3633
Saul Maldonado <samaldonado4@gmail.com>
2021-01-26 15:45:25 -0600

    feat: Add new java solution

--- strings/wildcard-matching/wildcard-matching.java
+++ strings/wildcard-matching/wildcard-matching.java
@@ -30,3 +30,37 @@ class Solution {
     return dp[S - 1][P - 1];
   }
 }
+
+class Solution {
+  public boolean isMatch(String s, String p) {
+    int i = 0;
+    int j = 0;
+    int prevI = 0;
+    int prevJ = -1; // init. to -1 until we find the first *
+
+    while (i < s.length()) {
+      if (j < p.length() && (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))) { // characters match or p is at ?
+        i++;
+        j++;
+      } else if (j < p.length() && p.charAt(j) == '*') { // p is ?
+        prevJ = j; // set backtracking index for j
+        prevI = i; // set backtracking index for i
+        j++; // increment p pointer only. greedy approach, match as little characters with *
+             // as possible
+      } else if (prevJ != -1) { // we can backtrack to a *
+        prevI++;
+        i = prevI; // backtrack and this time increment i
+
+        p = prevJ + 1; // backtrack and increment to next character
+      } else {
+        return false; // if we are at the end of p, the strings do not match
+      }
+    }
+
+    while (j < p.length() && p.charAt(j) == '*') { // at end of s, check for * at end of p
+      j++;
+    }
+
+    return j == p.length(); // we are at the end of p
+  }
+}
