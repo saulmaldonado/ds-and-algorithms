2bb2fdbe803bbedf111103bb3aea3d45bded6c58
Saul Maldonado <samaldonado4@gmail.com>
2021-02-02 21:45:35 -0600

    fix: Add missing ts solution

---
+++ arrays/best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,11 @@
+function maxProfit(prices: number[]): number {
+  let profit: number = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return profit;
+}
