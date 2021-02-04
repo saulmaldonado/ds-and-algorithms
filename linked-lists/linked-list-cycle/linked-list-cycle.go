47a6234d35fdb3c7322b4bcd39f1429e019e36ac
Saul Maldonado <samaldonado4@gmail.com>
2021-02-04 02:09:30 -0600

    feat: Add go solution

---
+++ linked-lists/linked-list-cycle/linked-list-cycle.go
@@ -0,0 +1,21 @@
+package linkedlistcycle
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
