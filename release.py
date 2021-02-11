2a624e2347c02f034def0a0adf933010a96a9b04
Saul Maldonado <samaldonado4@gmail.com>
2021-02-11 02:34:17 -0600

    refactor: Refactor and link release script

--- release.py
+++ release.py
@@ -1,32 +1,39 @@
-from typing import Match
+from typing import Match, Tuple, Union
 from urllib.request import urlopen, Request, HTTPError
+from string import Formatter
 import os
 import json
 import re
 
 
-event_path = os.environ["GITHUB_EVENT_PATH"]
-token = os.environ["GITHUB_TOKEN"]
-release_endpoint = (
+EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
+TOKEN = os.environ["GITHUB_TOKEN"]
+RELEASE_ENDPOINT = (
     "https://api.github.com/repos/saulmaldonado/ds-and-algorithms/releases"
 )
+FILES_ENDPOINT = (
+    "https://api.github.com/repos/saulmaldonado/ds-and-algorithms/pulls/{}/files"
+)
+
+MARKDOWN_LINK = "[{}](https://github.com/saulmaldonado/ds-and-algorithms/tree/main/{})"
 
 
 class PullRequest:
-    def __init__(self):
+    def __init__(self, event_path: str, files_endpoint: Formatter):
+        self.files_endpoint = files_endpoint
         self.event_path = event_path
         self.event = self.__get_event_payload()
         self.type, self.title, self.number = self.__get_ref()
         self.path = self.__get_path()
 
     def __get_event_payload(self):
-        with open(self.event_path) as fp:
-            return json.load(fp)
+        with open(self.event_path) as file_pointer:
+            return json.load(file_pointer)
 
-    def __get_path(self):
-        file_endpoint = f"https://api.github.com/repos/saulmaldonado/ds-and-algorithms/pulls/{self.number}/files"
+    def __get_path(self) -> Union[str, None]:
+        files_url = self.files_endpoint.format(self.number)
 
-        files = json.load(urlopen(file_endpoint))
+        files = json.load(urlopen(files_url))
 
         filenames = [file["filename"] for file in files]
 
@@ -35,22 +42,24 @@ class PullRequest:
                 r"(?P<topic>[\w]+)\/(?P<name>[\w!-)\-]+)\/(?P<filename>README\.md|\2(?:\.js|\.ts|\.go|\.java))",
                 filename,
             )
-            if match != None:
+            if match is not None:
                 return f"{match.group('topic')}/{match.group('name')}"
+            return None
 
     def __get_ref(self) -> str:
         ref = self.event["pull_request"]["head"]["ref"]
         number = self.event["number"]
         match = re.match(r"(?P<branch_type>feat|fix)\/(?P<name>[\w!-)\-]+)", ref)
-        if match == None:
+        if match is None:
             raise Exception(
                 "Pull request ref does not match convention, (feat|fix)/(branch name)"
             )
 
-        title = self.__format_title(match)
+        title = PullRequest.format_title(match)
         return (match.group("branch_type"), title, number)
 
-    def __format_title(self, match: Match) -> str:
+    @staticmethod
+    def format_title(match: Match) -> str:
         branch_type = match.group("branch_type")
         name = match.group("name")
 
@@ -62,10 +71,11 @@ class PullRequest:
 
 
 class Version:
-    def __init__(self, release_endpoint):
-        self.__get_recent_tag(release_endpoint)
+    def __init__(self, release_endpoint: str):
+        self.major, self.minor = Version.get_recent_version(release_endpoint)
 
-    def __get_recent_tag(self, release_endpoint):
+    @staticmethod
+    def get_recent_version(release_endpoint) -> Tuple[int, int]:
         recent_tag = ""
 
         releases = json.load(urlopen(release_endpoint))
@@ -77,8 +87,7 @@ class Version:
 
         match = re.match(r"v(?P<major>[\d]+)\.(?P<minor>[\d]+)", recent_tag)
 
-        self.major = int(match.group("major"))
-        self.minor = int(match.group("minor"))
+        return int(match.group("major")), int(match.group("minor"))
 
     def bump_major(self):
         self.major += 1
@@ -98,14 +107,19 @@ class Version:
 
 
 class Release:
-    def __init__(self, release_endpoint):
-        pr = PullRequest()
-        v = Version(release_endpoint)
-        v.bump_to_next(pr)
+    def __init__(
+        self, release_endpoint: str, mardown_link: Formatter, pull_req: PullRequest
+    ):
+        self.release_endpoint = release_endpoint
+        self.pull_req = pull_req
+        self.markdown_link = mardown_link
 
-        self.tag_name = v.get_tag()
-        self.name = pr.title
-        self.body = self.get_body(pr.path)
+        ver = Version(self.release_endpoint)
+        ver.bump_to_next(self.pull_req)
+
+        self.tag_name = ver.get_tag()
+        self.name = self.pull_req.title
+        self.body = self.get_body(self.pull_req.path)
 
     def get_payload(self):
         return json.dumps(
@@ -113,13 +127,13 @@ class Release:
         )
 
     def get_body(self, path):
-        if path == None:
+        if path is None:
             return ""
-        return f"[{self.name}](https://github.com/saulmaldonado/ds-and-algorithms/tree/main/{path})"
+        return self.markdown_link.format(self.name, path)
 
-    def new_release(self):
+    def new_release(self, token):
         req = Request(
-            release_endpoint,
+            self.release_endpoint,
             self.get_payload().encode("utf-8"),
             {
                 "Authorization": "Bearer " + token,
@@ -136,5 +150,6 @@ class Release:
 
 
 if __name__ == "__main__":
-    release = Release(release_endpoint)
-    release.new_release()
+    pr = PullRequest(EVENT_PATH, FILES_ENDPOINT)
+    release = Release(RELEASE_ENDPOINT, MARKDOWN_LINK, pr)
+    release.new_release(TOKEN)
