a3a188d9b1201352f4587ccaa21e1002308ffc6b
Saul Maldonado <samaldonado4@gmail.com>
2021-02-12 23:56:03 -0600

    feat: Add java solution

---
+++ arrays/contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+    for (int num : nums) {
+      if (set.contains(num)) {
+      }
+
+      set.add(num);
+    }
+    return false;
+  }
+}
