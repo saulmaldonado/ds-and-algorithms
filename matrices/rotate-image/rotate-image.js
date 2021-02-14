ae448de84fb7cc8e7a5f6f005e7316a9a176fe50
Saul Maldonado <samaldonado4@gmail.com>
2021-02-13 20:56:58 -0600

    feat: Add js solution

---
+++ matrices/rotate-image/rotate-image.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix) {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < ~~(n / 2); j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][n - j - 1];
+      matrix[i][n - j - 1] = temp;
+    }
+  }
+}
