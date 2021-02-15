f4aff39bbee68743338ee8411ffa7392180fc239
Saul Maldonado <samaldonado4@gmail.com>
2021-02-14 18:15:09 -0600

    feat: Add a js solution

---
+++ binary-search/sqrtx/sqrtx.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+function mySqrt(x) {
+  if (x < 2) {
+    return x;
+  }
+
+  let left = 1;
+  let right = x;
+
+  while (left < right) {
+    let mid = left + ~~((right - left) / 2);
+
+    let sq = mid * mid;
+
+    if (sq === x) {
+      return mid;
+    }
+
+    if (sq > x) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left - 1;
+}
