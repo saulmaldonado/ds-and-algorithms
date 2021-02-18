dd00b431a53cf6a5bbb1e8e5da7c06156cbda005
Saul Maldonado <samaldonado4@gmail.com>
2021-02-18 02:57:56 -0600

    feat: Add ts solution

---
+++ arrays/insert-interval/insert-interval.ts
@@ -0,0 +1,30 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  const ans: number[][] = [];
+
+  const n = intervals.length;
+  let i = 0;
+
+  while (i < n && intervals[i][1] < newInterval[0]) {
+    ans.push(intervals[i]);
+    i++;
+  }
+
+  while (i < n && intervals[i][0] <= newInterval[1]) {
+    if (intervals[i][0] < newInterval[0]) {
+      newInterval[0] = intervals[i][0];
+    }
+
+    if (intervals[i][1] > newInterval[1]) {
+      newInterval[1] = intervals[i][1];
+    }
+    i++;
+  }
+
+  ans.push(newInterval);
+
+  while (i < n) {
+    ans.push(intervals[i]);
+    i++;
+  }
+  return ans;
+}
