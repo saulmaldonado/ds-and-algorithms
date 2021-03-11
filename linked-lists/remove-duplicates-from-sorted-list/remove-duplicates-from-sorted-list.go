bc5b6b8ce8608b7c2f3b04baac59f1683c0a8c25
Saul Maldonado <samaldonado4@gmail.com>
2021-03-10 18:10:12 -0600

    feat: Add go solution

---
+++ linked-lists/remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,23 @@
+package removeduplicatesfromsortedlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	curr := head
+
+	for curr.Next != nil {
+		if curr.Val == curr.Next.Val {
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	return head
+}
