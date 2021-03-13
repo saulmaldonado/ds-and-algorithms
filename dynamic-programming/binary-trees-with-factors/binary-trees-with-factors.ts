29a1c150506d9a43b00cbef4a831c2edeea17820
Saul Maldonado <samaldonado4@gmail.com>
2021-03-13 17:04:39 -0600

    feat: Add ts solution

---
+++ dynamic-programming/binary-trees-with-factors/binary-trees-with-factors.ts
@@ -0,0 +1,36 @@
+function numFactoredBinaryTrees(arr: number[]): number {
+  const n: number = arr.length;
+
+  if (n === 1) {
+    return 1;
+  }
+
+  const mod = 1e9 + 7;
+  const map: Record<number, number> = {};
+
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < n; i++) {
+    map[arr[i]] = i;
+  }
+
+  const dp: number[] = new Array(n).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i < n; i++) {
+    let count = 1;
+    for (let j = 0; j < i; j++) {
+      if (arr[i] % arr[j] === 0 && Math.floor(arr[i] / arr[j]) in map) {
+        count += dp[j] * dp[map[Math.floor(arr[i] / arr[j])]];
+      }
+    }
+    dp[i] = count;
+  }
+  let sum = 0;
+
+  for (let num of dp) {
+    sum += num;
+  }
+
+  return sum % mod;
+}
