f11bc9c0dd0773e72a24442afb0ab2a39dde7659
Saul Maldonado <samaldonado4@gmail.com>
2021-04-05 23:56:39 -0500

    feat: Add reconstruct-original-digits-from-english ts solution

---
+++ strings/reconstruct-original-digits-from-english/reconstruct-original-digits-from-english.ts
@@ -0,0 +1,41 @@
+function originalDigits(s: string): string {
+  const count: number[] = new Array(10).fill(0);
+  for (const c of s) {
+    if (c == 'z') {
+      count[0]++;
+    } else if (c == 'w') {
+      count[2]++;
+    } else if (c == 'u') {
+      count[4]++;
+    } else if (c == 'x') {
+      count[6]++;
+    } else if (c == 'g') {
+      count[8]++;
+    } else if (c == 's') {
+      count[7]++;
+    } else if (c == 'h') {
+      count[3]++;
+    } else if (c == 'f') {
+      count[5]++;
+    } else if (c == 'i') {
+      count[9]++;
+    } else if (c == 'o') {
+      count[1]++;
+    }
+  }
+
+  count[7] -= count[6];
+  count[5] -= count[4];
+  count[3] -= count[8];
+  count[9] = count[9] - count[8] - count[5] - count[6];
+  count[1] = count[1] - count[0] - count[2] - count[4];
+
+  const sb: number[] = [];
+
+  for (let i = 0; i < count.length; i++) {
+    for (let j = 0; j < count[i]; j++) {
+      sb.push(i);
+    }
+  }
+  return sb.join('');
+}
