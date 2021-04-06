7174aebbc31d5e7bf6abd467683af31a3f781cfc
Saul Maldonado <samaldonado4@gmail.com>
2021-04-05 19:57:18 -0500

    feat: Add ones-and-zeroes js solution

---
+++ dynamic-programming/ones-and-zeroes/ones-and-zeroes.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+function findMaxForm(strs, m, n) {
+  const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
+
+  for (const s of strs) {
+    let ones = 0;
+    let zeros = 0;
+
+    for (const c of s) {
+      if (c == '1') {
+        ones++;
+      } else {
+        zeros++;
+      }
+    }
+
+    for (let i = m; i >= zeros; i--) {
+      for (let j = n; j >= ones; j--) {
+        dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+      }
+    }
+  }
+  return dp[m][n];
+}
